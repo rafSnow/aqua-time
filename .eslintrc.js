19e6c0bc21e63a68b7eb38a208f75a7099c4eb3f
Rafael <rafael.neves.souzaa@gmail.com>
2025-10-25 19:00:09 -0300

    feat: configuração inicial do projeto

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  root: true,
+  extends: [
+    '@react-native-community',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  rules: {
+    'prettier/prettier': 'warn',
+  },
+};
---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  arrowParens: "avoid",
+  bracketSameLine: true,
+  bracketSpacing: true,
+  singleQuote: true,
+  trailingComma: "all",
+  semi: true,
+  tabWidth: 2,
+  printWidth: 100,
+};
